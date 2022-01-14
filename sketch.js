ecd37090c3bff1baa7eaa3f075387952652e44a1
Cody <cody.tian@icloud.com>
2022-01-14 10:46:52 -0800

    add vehicle. add seek

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 /*
-@author
-@date 2022-01-
+@author Cody
+@date 2022-01-14
 
+coding plan
+    basic ☒ vehicle class with  show, ☒ applyForce, ☒ update
+    ☒☐☐ seek
 
  */
-let font
+let font, vehicle, gravity
+let target
 
 function preload() {
     font = loadFont('data/meiryo.ttf')
@@ -13,8 +17,20 @@ function preload() {
 function setup() {
     createCanvas(640, 360)
     colorMode(HSB, 360, 100, 100, 100)
+    vehicle = new Vehicle(random(width), random(height))
+    gravity = new p5.Vector(0, 0.1)
 }
 
-function draw() {    
+function draw() {
     background(234, 34, 24)
+    // fill with a green color for our target
+    fill(0, 100, 100);
+    noStroke()
+    target = createVector(mouseX, mouseY)
+    circle(target.x, target.y, 32)
+
+    vehicle.show()
+    // vehicle.applyForce(gravity)
+    vehicle.update()
+    vehicle.seek(target)
 }
---
+++ vehicle.js
@@ -0,0 +1,55 @@
+// A basic vehicle which is our path follower.
+
+
+class Vehicle {
+    constructor(x, y) {
+        // our natural position, velocity, acceleration
+        this.pos = new p5.Vector(x, y)
+        this.vel = new p5.Vector(0, 0)
+        this.acc = new p5.Vector(0, 0)
+        // our radius
+        this.r = 16
+        // our maximum speed and force
+        this.maxSpeed = 4
+        this.maxForce = 0.1
+    }
+
+    // seeks a target
+    seek(target) {
+        // our desired velocity is a straight line pointing from us to the
+        // target at our maximum speed
+        let desired = p5.Vector.sub(target, this.pos)
+        desired.setMag(this.maxSpeed)
+        // steering = desired - vel, Creg Renold's formula
+        let steering = p5.Vector.sub(desired, this.vel)
+        steering.limit(this.maxForce)
+        this.applyForce(steering);
+    }
+
+    show() {
+        fill(0, 0, 100)
+        noStroke()
+        // as always, remember that the third argument is the diameter, not
+        // the radius!
+        // circle(this.pos.x, this.pos.y, this.r*2)
+
+        // make a triangle
+        push()
+        translate(this.pos.x, this.pos.y)
+        rotate(this.vel.heading())
+        triangle(-this.r, -this.r/2, -this.r, this.r/2, this.r, 0)
+        pop()
+    }
+
+    applyForce(f) {
+        // F = ma. In this world, m = 1, so a = F.
+        this.acc.add(f)
+    }
+
+    update() {
+        this.vel.add(this.acc)
+        this.pos.add(this.vel)
+        // set our acceleration back to 0
+        this.acc = new p5.Vector(0, 0)
+    }
+}
